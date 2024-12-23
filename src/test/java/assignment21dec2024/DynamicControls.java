6c18ac7185562067e1e583bb0e26b37c782bb60a
Manu <Manu@Manu786>
2024-12-22 23:47:42 -0500

    assignment 21dec2024 done

--- src/test/java/assignment21dec2024/DynamicControls.java
+++ src/test/java/assignment21dec2024/DynamicControls.java
@@ -13,12 +13,15 @@ public class DynamicControls extends BaseTest{
 		
 		//click on Dynamic Controls link
 		driver.findElement(By.linkText("Dynamic Controls")).click();
+		//driver.findElement(By.xpath("//a[text()='Dynamic Controls']")).click();
 
 		//click on 'Remove' button
-		driver.findElement(By.xpath("//*[@id='checkbox-example']/button")).click();
-		
+		//driver.findElement(By.xpath("//*[@id='checkbox-example']/button")).click();
+		driver.findElement(By.xpath("//*[text()='Remove']")).click();  // xpath("//*[@onclick='swapCheckbox()']")
+
 		//verify expected "It's gone!" message with actual message
-		String message=driver.findElement(By.xpath("//*[@id='message']")).getText();
+		String message=driver.findElement(By.id("message")).getText();
+		//////String message=driver.findElement(By.xpath("//p[text()='It's gone!']")).getText();
 		Assert.assertEquals(message, "It's gone!");   
 		
 	}
--- src/test/java/assignment21dec2024/JavascriptEx.java
+++ src/test/java/assignment21dec2024/JavascriptEx.java
@@ -13,8 +13,10 @@ public class JavascriptEx extends BaseTest {
 	@Test
 	public void loginTest() {
 
+		// open url
 		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
+		// enter username and password
 		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
 		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
 		// driver.findElement(By.xpath("//button[@type='submit']")).click();
@@ -28,12 +30,28 @@ public class JavascriptEx extends BaseTest {
 		// WebElement pwdText=driver.findElement(By.name("password"));
 		// js.executeScript("arguments[0].value='admin123';",pwdText);
 
+		// click on login button using JavascriptExecutor
 		WebElement loginBtn = driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
 		js.executeScript("arguments[0].click();", loginBtn);
 
+		// find url of landing page or homepage
 		String url = js.executeScript("return document.URL").toString();
 		System.out.println(url);
+
+		// verify the actual url with expected url
 		Assert.assertEquals(url, "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+
+		// reload landing page
+		js.executeScript("location.reload();");
+		// driver.navigate().refresh();
+
+		// Use JavaScriptExecutor to scroll the element into view
+		WebElement webEle = driver.findElement(By.xpath("//*[@id='app']/div[1]/div[2]/div[2]/div/div[7]/div/div[1]/div/p"));
+		js.executeScript("arguments[0].scrollIntoView(true);", webEle);
+		
+		//js.executeScript("window.scrollBy(0,1000);");  //vertical scroll
+		
+		Assert.assertEquals(webEle.getText(), "Employee Distribution by Location");
 
 	}
 }
